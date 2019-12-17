19c86f519398284f9f111d4c81eb773a68042aea
Khoronus <antigorite@hotmail.com>
2019-12-17 13:37:36 +0900

    Add:  - Batch file to clear the cache.

---
+++ configure/win/900. clear cache.bat
@@ -0,0 +1,10 @@
+SET F="..\..\CmnCS\build"
+IF EXIST %F% RMDIR /S /Q %F%
+SET F="..\..\CmnIO\build"
+IF EXIST %F% RMDIR /S /Q %F%
+SET F="..\..\CmnIP\build"
+IF EXIST %F% RMDIR /S /Q %F%
+SET F="..\..\CmnLib\build"
+IF EXIST %F% RMDIR /S /Q %F%
+SET F="..\..\CmnMath\build"
+IF EXIST %F% RMDIR /S /Q %F%
